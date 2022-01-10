affdc97da2c90d1dc15c1a972df2793ef593b482
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2022-01-10 06:42:51 -0600

    change data

--- src/index.js
+++ src/index.js
@@ -21,5 +21,5 @@ async function runApp() {
 
 runApp().catch((error) => {
   Logger.error(error);
-  process.exit(1); // eslint-disable-line no-process-exit
+  //process.exit(1); // eslint-disable-line no-process-exit
 });
